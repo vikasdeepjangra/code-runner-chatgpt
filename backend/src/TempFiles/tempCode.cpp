dd810f51674593b8e705e9888f7998afeccf9416
vikasdeepjangra <vikasdeepjangra@gmail.com>
2023-05-02 17:03:15 +0530

    codeprac

--- backend/src/TempFiles/tempCode.cpp
+++ backend/src/TempFiles/tempCode.cpp
@@ -1,1 +1,11 @@
-//Type your code here.
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int i;
+    cout<<"Enter the value of i: ";
+    cin>>i;
+    cout<<"Value of i is: "<<i<<endl;
+    return 0;
+}
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,9 +4,11 @@ import bodyParser from "body-parser";
 import fs from 'fs';
 const os = require("os");
 const pty = require("node-pty");
+const OpenAI = require('openai-api');
 
 const { exec } = require('child_process'); 
 const shell = os.platform() === "win32" ? "powershell.exe" : "bash";
+const openai = new OpenAI('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');
 
 const io = require('socket.io')(3000, {
     cors: {
@@ -23,6 +25,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended:true
 }))
+
+let ptyProcess = pty.spawn(shell, [], {
+    cwd: __dirname,
+    env: process.env
+});
 
 function createTempFileWithCode(code){
     return new Promise(function (resolve) {
@@ -49,15 +56,9 @@ function createExectuableFile(){
     })
 }
 
-
 io.on('connection', async socket => { 
     console.log("Connection Formed!")
     console.log(socket.id);
-
-    let ptyProcess = pty.spawn(shell, [], {
-        cwd: __dirname,
-        env: process.env
-    });
     
     socket.on("compile-code", async (code) => {
         await createTempFileWithCode(code);
@@ -67,15 +68,40 @@ io.on('connection', async socket => {
             .catch(err => socket.emit("compile-code-msg", err));
     })
 
-    socket.on("run-code", () => {
-        ptyProcess.on('data', function (output){
-            socket.emit("code-output", output)
-        });
+    socket.on("run-code", () => {    
         ptyProcess.write("cls\r");
-        ptyProcess.write("./TempFiles/outputFile.exe\r");
+        ptyProcess.write("./TempFiles/outputFile.exe\r");    
     })
+
+    ptyProcess.on('data', function (output){
+        console.log(output)
+        socket.emit("code-output", output)
+    });
 
     socket.on("send-input-value", (data) => {
         ptyProcess.write(data);
     });
+
+    socket.on("debug-code", async (code) => {
+try {
+    const completion = await openai.complete({
+        engine: 'text-davinci-003',
+        prompt:`fix this c++ code \n${code}`,
+        temperature:0,
+        maxTokens:150,
+        n: 1,
+        frequency_penalty:0,
+        presence_penalty:0.6,
+        stop:[" Human:", " AI:"]
+    });
+
+    const { choices } = completion.data;
+        socket.emit('gpt-response', choices[0].text);
+        console.log(choices[0].text);
+    } catch (error) {
+    console.log(error);
+    socket.emit('gpt-response', error);
+    }
+    })
+
 })
